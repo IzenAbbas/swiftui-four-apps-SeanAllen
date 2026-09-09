6270cf94dfd35614b88e04d716ea605683c37a0d
IzenAbbas <izen.khokhar@gmail.com>
2026-09-09 18:31:23 +0500

    first commit

---
+++ weather/weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Ali Abbas on 09/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
