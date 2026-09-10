6bfc2d790a5ea86f2020d9c57068dcbf035104ff
IzenAbbas <izen.khokhar@gmail.com>
2026-09-10 18:07:00 +0500

    feat(apple_framework): initialize apple_framework project and set deployment target for weather app

---
+++ apple_framework/apple_framework/apple_frameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworkApp.swift
+//  apple_framework
+//
+//  Created by Ali Abbas on 10/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
